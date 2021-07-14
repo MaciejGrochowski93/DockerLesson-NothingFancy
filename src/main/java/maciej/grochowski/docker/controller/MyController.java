e3dffe14b80945e1deb96cae496d64cd2c82dbd8
Maciej <maciejgrochowski93@o2.pl>
2021-07-14 18:35:56 +0200

    adding Dockerfile_prod/test & a variable ${envName} dependent on the particular file

--- src/main/java/maciej/grochowski/docker/controller/MyController.java
+++ src/main/java/maciej/grochowski/docker/controller/MyController.java
@@ -1,13 +1,17 @@
 package maciej.grochowski.docker.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "heyeyeyeyeyey";
+    @Value("${envName}")
+    private String envName;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "heyeyeyeyeyey " + envName;
     }
 }
